0ff9ea88a7878fd66992d19fed7cae757b5280ec
abnemm <abnemartins15@gmail.com>
2023-01-12 12:08:09 -0300

    Pokedex subiu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Pokemon } from './interfaces/Pokemon';
 
 
 export interface PeriodicElement {
@@ -15,18 +16,7 @@ interface tabela{
   imagem: string
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+
 
 @Component({
   selector: 'app-root',
@@ -37,16 +27,15 @@ export class AppComponent {
   
   //variaveis
   title = 'Pokedex';
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
   apiURL = "https://pokeapi.co/api/v2/";
-  listaPokemon : any;
+  listaPokemon : Array<any> = [];
   colunas: string[]=['Número','Nome','imagem']
   
-  dados: tabela[] = [
-    {numero: "132", nome: "Ditto", imagem:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"},
-    {numero: "4", nome: "Charmander", imagem:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"},
-    {numero: "a", nome: "Buizel", imagem:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/418.png"}
+
+  dados: Pokemon[] = [
+    // {numero: "132", nome: "Ditto", link:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"},
+    // {numero: "4", nome: "Charmander", link:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"},
+    // {numero: "a", nome: "Buizel", link:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/418.png"}
   ]
   
 
@@ -60,7 +49,7 @@ export class AppComponent {
   }
 
   funcaoListarTodosPokemons() {
-    this.http.get(`${ this.apiURL }pokemon`)
+    this.http.get(`${ this.apiURL }pokemon?limit=151`)
     .subscribe((resultado: any) => {
       this.listaPokemon = resultado.results;
       console.log(resultado.results.value) 
@@ -68,11 +57,23 @@ export class AppComponent {
   }
 
   funcaoPreencheArray(){
-    
-    console.log("entrou");
-    console.log(this.listaPokemon)
 
-    // for (let index = 0; this.listaPokemon.lenght < 20; index++) {
+    //função map 
+    this.listaPokemon = this.listaPokemon.map((value: any) => {
+      this.http.get(`${ this.apiURL }pokemon/${value.name}`)
+      .subscribe((resultado: any) => { 
+        value.link = resultado.sprites.front_default;
+      })
+      return value;
+    }  
+    );
+    console.log(this.listaPokemon);
+    this.dados = this.listaPokemon;
+
+    // this.listaPokemon.forEach((element: Pokemon)=>{
+      
+    // });
+    // for (let index = 0; index < this.listaPokemon.length; index++) {
     //   console.log("Numero", index +1);
     //   console.log(this.listaPokemon[index].name);
     // }
